2a8ecbfc94038aa9fe587087ae2a91f8b78c87b1
Александр <U4iha312@mail.ru>
2022-10-04 02:57:30 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='justify-content-center'>Hello</div>
+    </div>
+  );
+}
+
+export default App;
