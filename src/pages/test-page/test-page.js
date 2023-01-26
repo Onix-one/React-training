b47ee17f898925265ee9e204c8f0aa69f7732a0e
Aliaksandr Anikeyeu <a.anikeyeu@outlook.com>
2023-01-27 01:30:18 +0300

    Add changes

--- src/pages/test-page/test-page.js
+++ src/pages/test-page/test-page.js
@@ -2,6 +2,6 @@ import { Component } from "react";
 
 export default class TestPage extends Component {
   render() {
-    return <div className="col-12">Hi</div>;
+    return <div className="col-5">Hi</div>;
   }
 }
